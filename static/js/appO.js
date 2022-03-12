0041043bf652d551d73e9529b70d7c3fd42a607d
ambermiddendorf <ambermiddendorf@hotmail.com>
2022-03-12 10:51:08 -0600

    code complete

---
+++ static/js/appO.js
@@ -0,0 +1,47 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    //clear out table data
+    tbody.html("");
+    //Loop through each object in the data
+    //append a row and cells for each value in the row
+    data.forEach((dataRow)=> {
+        let row = tbody.append("tr");
+        //loop through each field in the dataRow
+        //add each value as a table cell (td)
+        Object.values(dataRow).forEach((val)=>{
+            let cell = row.append("td");
+            cell.text(val);
+    }
+    );
+    });
+};
+
+function handleClick() {
+    // grab the datetime value from the filter
+    let date=d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    //check to see if a date was entered
+    //filter the data using that date
+    if (date) {
+        //apply filter to the table data
+        // only keep rows where 'datetime' matches 
+        //the filter value
+        filteredData = filteredData.filter(row=> row.datetime === date);
+    };
+        //rebuild the table using the filtered data
+    //if no date filter was entered 
+    //the whole data table will be returned.
+        buildTable(filteredData);
+};
+
+// listen for the click button event
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the talbe when the page loads
+buildTable(tableData);
+
